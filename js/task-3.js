b4db6f30c733a5d0ebce4139e8c32e247ccbe02d
Anna <anna.yevchuk26@gmail.com>
2024-05-06 22:19:25 +0300

    done

--- js/task-3.js
+++ js/task-3.js
@@ -2,3 +2,7 @@ function checkForSpam(message) {
     return (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')
     );
 }
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("Get the best SALE offers now!")); // true
+console.log(checkForSpam("SPAM messages are blocked")); // true
+console.log(checkForSpam("Buy now")); // false
